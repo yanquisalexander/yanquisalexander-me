d48c240dedb22736c5242e51dda434d3e7853bc0
Alexander Barrios <alexander.barrios@estudiante.ceibal.edu.uy>
2024-05-04 23:18:09 -0300

    commit all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+import auth from "auth-astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.yanquisalexander.me",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), auth()],
+  output: "server",
+  adapter: vercel()
 });
