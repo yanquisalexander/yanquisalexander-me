0b15c8fbbd335b138913f271c5693a6d7d72d705
Alexander Barrios <yanquis.barrios@estudiantes.utec.edu.uy>
2024-06-05 10:37:00 -0300

    feat: Update GuestBook component to use Organization type in JSON-LD schema.org metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import auth from "auth-astro";
 import vercel from "@astrojs/vercel/serverless";
-
 import db from "@astrojs/db";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.yanquisalexander.me",
-  integrations: [tailwind(), sitemap(), auth(), db()],
+  integrations: [tailwind(), sitemap(), auth(), db(), react()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
